94c63b6b3335f612a89cd697cf46e372b49b8a44
mkoenigmcfarlane <mkoenigmcfarlane@gmail.com>
2016-03-24 11:18:13 -0500

    Initial commit
    loading template version 1.3.3 as of 3/24/16

---
+++ BarrWebMapExample/js/config/bookmarks.js
@@ -0,0 +1,56 @@
+define({
+	map: true,
+	editable: true,
+	bookmarks: [		
+		{
+			extent: {
+				xmin: -93.326723,
+				ymin: 45.121975,
+				xmax: -93.260076,
+				ymax: 45.146406,
+				spatialReference: {
+					wkid: 4326
+				}
+			},
+			name: 'Demo 1'
+		},
+		{
+			extent: {
+				xmin: -93.435070,
+				ymin: 45.214964,
+				xmax: -93.223068,
+				ymax: 45.293512,
+				spatialReference: {
+					wkid: 4326
+				}
+			},
+			name: 'Andover'
+		},
+		{
+			extent: {
+				xmin: -93.409734,
+				ymin: 45.139808,
+				xmax: -93.194299,
+				ymax: 45.217493,
+				spatialReference: {
+					wkid: 4326
+				}
+			},
+			name: 'Coon Rapids'
+		},
+		{
+			extent: {
+				xmin: -93.541020,
+				ymin: 45.224995,
+				xmax: -93.323868,
+				ymax: 45.301718,
+				spatialReference: {
+					wkid: 4326
+				}
+			},
+			name: 'Ramsey'
+		}
+	]
+});
+
+
